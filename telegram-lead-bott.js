0b75f632a2d46ca5f77d643bf3be76c48147fdf1
zufuxX <aymenboubaya7@gmail.com>
2026-03-22 12:29:44 +0100

    Update telegram-lead-bott.js

--- telegram-lead-bott.js
+++ telegram-lead-bott.js
@@ -17,7 +17,7 @@ app.listen(port, () => {
 // ============================================================
 const TELEGRAM_TOKEN   = process.env.TELEGRAM_TOKEN || '8766071458:AAHQ_P5uQ_dyusYsRnkEoKPsWCB6mEK8KY4';
 const WEBHOOK_URL      = process.env.WEBHOOK_URL || 'https://hook.eu2.make.com/ox7k377smi1srcw731gkij7vehoxr3h5';
-const CANAL_LINK       = 'https://t.me/+aB_kistPlmI3YTQ0';
+const CANAL_LINK       = 'https://t.me/+E8-N241k708zZGFk';
 const ID_LEO           = '1060253366'; 
 // ============================================================
 
